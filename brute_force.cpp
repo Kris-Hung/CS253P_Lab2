8f1edf1d36c41e91deb6131bb458c2ddaffec2e7
Kris-Hung <43719498+Kris-Hung@users.noreply.github.com>
2020-10-16 20:33:11 -0700

    Add files via upload

---
+++ brute_force.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+bool isSubStr(string s1, string s2) { // s2 is the long one
+	signed int size1 = s1.size(), size2 = s2.size();
+	for (int i = 0; i < size2 - size1; i++) {
+		if (s1 == s2.substr(i, s1.size())) {
+			return true;
+		}
+	} 
+	return false;
+	}
+
+
+
+void contain(vector<string> & input) {
+	int inputSize = input.size();
+	cout << "----------------" << endl;
+	for(int i =0; i < inputSize; i++) {
+		cout << input.at(i) << ": ";
+		for (int j = i+1; j < inputSize; j++) {
+			if (isSubStr(input.at(i), input[j])) {
+				cout << input[j] << ", ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+
+int main(int argc, char const *argv[]){
+	vector<string> input;
+	string str;
+	cout << "Input your strings: " << endl;
+	for (int i = 0; i < stoi(argv[1]); i++) {
+		getline(cin, str);
+		input.push_back(str);
+	}
+	contain(input);
+	return 0;
+}
